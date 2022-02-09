b1f5e6515d81083e54e3911e9f0bf911766a50d4
RicardoSolance <ricardontutumu@gmail.com>
2022-02-09 22:54:05 +0000

    menu hamburguesa no se ejecuta, pero si en el inspector

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 
 //cogemos el loogo ha
-document.getElementsByClassName("close-menu").onclick = function () { close() };
+// document.getElementById("close").onclick = function () { close() };
 
 
 
-function close(){ 
-    document.getElementById('pop').style.display="none";
+// function close(){
+//     document.getElementById('navegacion').style.display = "none";
+// }
+let x = document.getElementById("close");
+x.onclick = function () { close() };
+function close(){
+    document.getElementById("navegacion").style.display =none;
+}
 
+
+let open = document.getElementById("open-menu");
+open.onclick = function () { abrir() };
+
+function abrir() {
+    document.getElementById("navegacion").style.display =flex;
 }
